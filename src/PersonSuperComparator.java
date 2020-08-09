f78076d657f35e33c06037258126d6e3b626472a
Gulnaz Aizatullina <gulnazka.aiza@gmail.com>
2020-08-09 12:43:07 +0300

    Задача выполнена

--- src/PersonSuperComparator.java
+++ src/PersonSuperComparator.java
@@ -3,9 +3,9 @@ import java.util.Comparator;
 class PersonSuperComparator implements Comparator<Person> {
 
     @Override
-    public int compare(Person person1, Person person2) {
-        int namesCompare = person1.getName().compareTo(person2.getName());
-        int ageCompare = person1.getAge() - person2.getAge();
+    public int compare(Person p1, Person p2) {
+        int namesCompare = p1.getName().compareTo(p2.getName());
+        int ageCompare = p1.getAge() - p2.getAge();
 
         if (namesCompare == 0) {
             return ageCompare;
